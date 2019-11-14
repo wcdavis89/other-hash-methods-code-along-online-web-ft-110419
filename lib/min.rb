4d55bf70b0f89b74e875691a10baff52e85e6e03
Whittney Davis <wcdavis89@yahoo.com>
2019-11-14 23:47:04 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -6,7 +6,7 @@ require 'pry'
   vegetable: ["carrots", "broccoli", "cucumbers"],
   meat: ["chicken", "steak", "salmon"],
   grains: ["rice", "pasta"]
-
+}
 
 def get_the_min(groceries)
   groceries.values.flatten.min
